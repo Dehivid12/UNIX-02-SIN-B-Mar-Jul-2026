41f873868366e95cea7e9ce075a81e11e0ff9eee
Dehivid12 <dehividsalgado2022@gmail.com>
2026-03-23 13:51:54 +0000

    Validacion de firma

--- Practical_workshop_1.sh
+++ Practical_workshop_1.sh
@@ -9,3 +9,5 @@ gpg --import PD_llave_publica.asc
 echo "Te habla tu papi" > doc_no_cifrado.txt #echo imprime algo, entre comillas el mensaje, > toma el comando y lo envia a un archivo, y el final es el nombre
 gpg --output doc_cifrado.txt --encrypt --recipient 001610346A476C356D46ADE76C05276C92BBAFD2 doc_no_cifrado.txt
 gpg --decrypt doc_no_cifrado.txt
+gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt
+gpg --edit-key 
