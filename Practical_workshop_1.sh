789924a259c8910201009b333e5d5f66ef90a0f0
Dehivid12 <dehividsalgado2022@gmail.com>
2026-03-23 13:29:19 +0000

    Desencriptacion de mensaje

--- Practical_workshop_1.sh
+++ Practical_workshop_1.sh
@@ -8,3 +8,4 @@ gpg --armor --export-secret-keys
 gpg --import PD_llave_publica.asc
 echo "Te habla tu papi" > doc_no_cifrado.txt #echo imprime algo, entre comillas el mensaje, > toma el comando y lo envia a un archivo, y el final es el nombre
 gpg --output doc_cifrado.txt --encrypt --recipient 001610346A476C356D46ADE76C05276C92BBAFD2 doc_no_cifrado.txt
+gpg --decrypt doc_no_cifrado.txt
