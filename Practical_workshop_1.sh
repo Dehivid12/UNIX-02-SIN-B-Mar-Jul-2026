4b6f837bb47a81a1e6a953e1f28399e9c0daf568
Dehivid12 <dehividsalgado2022@gmail.com>
2026-03-23 13:15:20 +0000

    texto cifrado Mateo

--- Practical_workshop_1.sh
+++ Practical_workshop_1.sh
@@ -5,3 +5,6 @@ gpg --full-generate-key
 gpg --list-keys
 gpg --armor --export 
 gpg --armor --export-secret-keys
+gpg --import PD_llave_publica.asc
+echo "Te habla tu papi" > doc_no_cifrado.txt #echo imprime algo, entre comillas el mensaje, > toma el comando y lo envia a un archivo, y el final es el nombre
+gpg --output doc_cifrado.txt --encrypt --recipient 001610346A476C356D46ADE76C05276C92BBAFD2 doc_no_cifrado.txt
