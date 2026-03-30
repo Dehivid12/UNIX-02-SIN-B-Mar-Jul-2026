bb058a064fbd36c3e5bcd32b6f48b9b86fd7d4e3
Dehivid12 <dehividsalgado2022@gmail.com>
2026-03-30 13:45:24 +0000

    Create file and stat

--- practica_clase2.sh
+++ practica_clase2.sh
@@ -3,3 +3,5 @@ sudo apt upgrade
 sudo apt install parted
 sudo parted -l && echo -e "ln---ln" && lsblk -f && echo -e "ln---ln"
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
+echo "esto es un archivo">archivo.txt
+stat arhcivo.txt
