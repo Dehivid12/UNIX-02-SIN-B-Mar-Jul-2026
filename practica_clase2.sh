f212de3a7c53ce584a432781db9ec378b8b276ab
Dehivid12 <dehividsalgado2022@gmail.com>
2026-03-30 13:12:42 +0000

    If the first file is found it displays UEFI, otherwise it displays BIOS

--- practica_clase2.sh
+++ practica_clase2.sh
@@ -2,3 +2,4 @@ sudo apt update
 sudo apt upgrade
 sudo apt install parted
 sudo parted -l && echo -e "ln---ln" && lsblk -f && echo -e "ln---ln"
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
