36f39ed2d8c73cc4dc127c769edbc3b834d6983f
Dehivid12 <dehividsalgado2022@gmail.com>
2026-04-06 12:34:54 +0000

    Create proyecto and move to this file

---
+++ script_boot.sh
@@ -0,0 +1,7 @@
+cd #change directory
+cd /home/codespace #move root to main file
+cd ~ #Goes to the users home directory
+cd $home #Makes the same job as cd
+mkdir proyecto #create new file
+cd proyecto/ #move to this file
+
