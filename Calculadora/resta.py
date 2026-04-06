56d4ec6dd07c29adecb343c178c5fd2dc813b22f
Dehivid12 <dehividsalgado2022@gmail.com>
2026-04-06 12:15:28 +0000

    clean repo

--- Calculadora/resta.py
+++
@@ -1,2 +0,0 @@
-def restart(a,b): 
-    return a-b
--- practica_clase2.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install parted
-sudo parted -l && echo -e "ln---ln" && lsblk -f && echo -e "ln---ln"
-[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
-echo "esto es un archivo">archivo.txt
-stat arhcivo.txt
