0143352fc9c429606970a67b59a25186f3fbfe2c
Dehivid12 <dehividsalgado2022@gmail.com>
2026-04-15 13:36:29 +0000

    create an empty file and change its permissions

---
+++ ejercicio2.sh
@@ -0,0 +1,5 @@
+touch prueba.txt # creates an empty file
+chmod 600 prueba.txt # change permissions
+ls -l prueba.txt # show its permissions
+chmod 755 prueba.txt # # improve permissions
+ls -l prueba.txt # show new permissions
