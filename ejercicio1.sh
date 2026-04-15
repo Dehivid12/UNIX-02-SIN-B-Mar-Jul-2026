b3bf2c3219f2f17b8b4d6066e5fcc60f9787486b
Dehivid12 <dehividsalgado2022@gmail.com>
2026-04-15 13:30:49 +0000

    List of commands which need and dont need sudo

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -6,3 +6,7 @@ ls -l hola.sh # show hola.sh permissions
 chmod +x hola.sh # give execute permission 
 ls -l hola.sh # show again its permissions
 ./hola.sh # execute 
+ls /etc # no requiere sudo
+touch /etc/prueba.txt # requiere sudo porque infiere en un programa del root /
+mkdir ~/mi_carpeta # no altera nada dentro de root, sino de home
+apt install cowsay # requiere sudo porque instala algo directamente en el sistema
