ad813583a9f7ddc9f1796f251050f85333af0756
Dehivid12 <dehividsalgado2022@gmail.com>
2026-04-15 13:09:34 +0000

    Give permissions and execute hola.sh

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -2,3 +2,7 @@ echo '#!/bin/sh' > hola.sh # create hola.sh
 echo 'echo "Hola desde mi primer script"' >> hola.sh # adds text in the file
 cat hola.sh # show the files content
 sudo ./hola.sh # executes the file, needs permission
+ls -l hola.sh # show hola.sh permissions
+chmod +x hola.sh # give execute permission 
+ls -l hola.sh # show again its permissions
+./hola.sh # execute 
