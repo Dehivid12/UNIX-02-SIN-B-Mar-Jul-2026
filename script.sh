11062b7d5e08bf1552f44f05980effd11ce3af5a
Dehivid12 <dehividsalgado2022@gmail.com>
2026-04-15 12:38:04 +0000

    add content with >>

--- script.sh
+++ script.sh
@@ -26,3 +26,4 @@ sudo echo "hola" > /etc/archivo_protegido # it does not work because sudo only a
 echo "hola" | sudo tee /etc/archivo_protegido > /dev/null # create a protected file with "hola"
 echo "hola" | sudo tee /etc/archivo_protegido # opens the file in the terminal
 cat /etc/archivo_protegido # verify if the file exists and check its content
+sudo sh -c 'echo "chao" >> /etc/archivo_protegido' # add content in the file
