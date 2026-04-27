3a191c6b4dcf7dc0f4cb3ecc8f14a86b4dfc7d9b
Dehivid12 <dehividsalgado2022@gmail.com>
2026-04-27 13:22:42 +0000

    Create secreto.txt, and create new user

--- script27-4.sh
+++ script27-4.sh
@@ -4,4 +4,11 @@ mkdir directorio1 # creates new directory
 umask 027 # changes permissions
 touch archivo2 # Creates a new empty file called archivo2
 mkdir directorio2 # creates new directory (2)
-
+umask 077 # Give higher permissions
+touch secreto.txt # Create secreto.txt
+mkdir privado # creade directory "privado"
+umask 022 # original permissions
+echo "Hola" > mi_archivo
+ls -l mi_archivo
+sudo useradd -m -s /usr/bin/zsh luna # creates new user
+sudo chown luna mi_archivo # changes owner
