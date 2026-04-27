1f4401621f2bc14fb75beef1a749e3b897292325
Dehivid12 <dehividsalgado2022@gmail.com>
2026-04-27 12:54:06 +0000

    Create archivo1 and archivo2, their directories and change permissions

---
+++ script27-4.sh
@@ -0,0 +1,7 @@
+umask # change access levels
+touch archivo1 # Cretes a new empty file called archivo1
+mkdir directorio1 # creates new directory 
+umask 027 # changes permissions
+touch archivo2 # Creates a new empty file called archivo2
+mkdir directorio2 # creates new directory (2)
+
