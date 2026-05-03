309d3d333e006e13b3a849c972f409d88e07031d
Dehivid12 <dehividsalgado2022@gmail.com>
2026-05-03 13:56:22 +0000

    1-4

---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,11 @@
+ls # list of file information
+LS # syntaxis error
+ls -l # larger list
+ls -r # inverse alphabetical order
+pwd # shows current directory
+cd # change directory
+cd Documents # change directory to Documents
+cd / # change directory to root
+cd /home/sysadmin # change to principal directory
+cd School/art # change to Art directory (relative route)
+cd .. # go to a superior directory
