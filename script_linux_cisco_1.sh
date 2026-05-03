8610b1abbf3a9f5b73feb5da3aaef04a5418be8a
Dehivid12 <dehividsalgado2022@gmail.com>
2026-05-03 17:16:16 +0000

    11

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -9,3 +9,22 @@ cd / # change directory to root
 cd /home/sysadmin # change to principal directory
 cd School/art # change to Art directory (relative route)
 cd .. # go to a superior directory
+ls -lt # organize files
+-t # time
+-s # size
+-r # reverse
+su # act as different user
+exit # go back as original user
+sudo # privileged access
+ls -l # check for permissions
+chmod # change permissions of a file or directory
+chown # change owner (can only be done by owner or root user)
+cat # quickly see the content of a file
+head # parts at the beggining
+tail # parts at the end
+cp # copy and paste in a differnet location
+dd # same as cp, but at bit level
+
+
+
+
