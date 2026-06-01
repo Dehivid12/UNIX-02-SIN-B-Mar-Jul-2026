fcea3a8a66c921af9157c26b87d26a70d2d2405a
Dehivid12 <dehividsalgado2022@gmail.com>
2026-06-01 13:17:54 +0000

    Depuration system

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x   # Shebang: define el intérprete (Bash) y activa el modo de depuración (-x) para mostrar cada comando antes de ejecutarse.bash --version
+set -x
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
@@ -6,3 +7,5 @@ echo ${OSTYPE}
 ps -ef
 df -h
 bash -r # execute in restricted mode
+bash -n blackhatbash1.sh # debugger
+set +x
