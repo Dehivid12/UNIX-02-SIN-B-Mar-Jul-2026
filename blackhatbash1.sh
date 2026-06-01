75c73daa894c937adc7bf63e9baf0f1f7cc402ae
Dehivid12 <dehividsalgado2022@gmail.com>
2026-06-01 13:02:20 +0000

    Restricted mode

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-bash --version
+#!/bin/bash -x   # Shebang: define el intérprete (Bash) y activa el modo de depuración (-x) para mostrar cada comando antes de ejecutarse.bash --version
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
 ps -ef
 df -h
+bash -r # execute in restricted mode
