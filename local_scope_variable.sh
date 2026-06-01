b259416322dc7e24387d1c4cc3545b101abde5dc
Dehivid12 <dehividsalgado2022@gmail.com>
2026-06-01 13:51:11 +0000

    Local and global variables

--- local_scope_variable.sh
+++ local_scope_variable.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -x
-PUBLISHER="No Starch Press"
+PUBLISHER="No Starch Press" # Global variable: accesible through out all the script
 print_name(){
- local name
- name="Black Hat Bash"
- echo "${name} by ${PUBLISHER}"
+    local name                  # Local variable: only exists inside the function
+    name="Black Hat Bash"
+    echo "${name} by ${PUBLISHER}"
 }
 print_name
 echo "Variable ${name} will not be printed because it is a local variable."
-
 set +x
