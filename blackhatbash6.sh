365c1ec2b20dcb921f0fdbaba6097468901c2a34
Dehivid12 <dehividsalgado2022@gmail.com>
2026-06-08 12:57:05 +0000

    Argumentos

--- blackhatbash6.sh
+++ blackhatbash6.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-ls -l / grep "bin"
+ls -l / | grep "bin"
 SCRIPT_NAME="${0}"
 TARGET="${1}"
 
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-
-if timeout 3 bash -c "echo > /dev/tcp/${TARGET}/80" 2>/dev/null; then
-    echo "Target ${TARGET} is reachable."
-else
-    echo "Target ${TARGET} is NOT reachable."
-fi
 echo "The arguments are: $@"
 echo "The total number of arguments are: $#"
-ping -c 3 "${TARGET}"
+
+ping -c 2 "${TARGET}"
