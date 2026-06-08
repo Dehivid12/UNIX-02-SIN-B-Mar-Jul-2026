7a6741c9550bd350e4302bc4287664551f817478
Dehivid12 <dehividsalgado2022@gmail.com>
2026-06-08 12:49:06 +0000

    ping -c 3

--- blackhatbash6.sh
+++ blackhatbash6.sh
@@ -2,6 +2,15 @@
 ls -l / grep "bin"
 SCRIPT_NAME="${0}"
 TARGET="${1}"
+
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}" 
+
+if timeout 3 bash -c "echo > /dev/tcp/${TARGET}/80" 2>/dev/null; then
+    echo "Target ${TARGET} is reachable."
+else
+    echo "Target ${TARGET} is NOT reachable."
+fi
+echo "The arguments are: $@"
+echo "The total number of arguments are: $#"
+ping -c 3 "${TARGET}"
