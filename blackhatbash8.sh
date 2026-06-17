eae69a7348d97db13c7a818c8ac4244bc9039144
Dehivid12 <dehividsalgado2022@gmail.com>
2026-06-17 12:57:22 +0000

    blackhatbash8 remove spaces, and change Mozilla for Godzilla

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -6,3 +6,8 @@ awk '{print $1,$NF}' log.txt
 awk -F',' '{print $1}' example_csv.txt
 awk 'NR < 10' log.txt
 grep "42.236.10.117" log.txt | awk '{print $7}'
+sed 's/Mozilla/Godzilla/g' log.txt
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep -o "Godzilla" log.txt # Looks only for the word "Godzilla" in log.txt
+grep -o "Godzilla" newlog.txt # Looks only for the word "Godzilla" in newlog.txt
+sed 's/ //g' log.txt > newlog1.txt
